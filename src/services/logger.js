6cb3f60cfda510f46d9bfd79bf001fa17323e367
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-08-03 18:05:45 -0300

    feat: create looger.js at services folder

---
+++ src/services/logger.js
@@ -0,0 +1,13 @@
+import winston from "winston";
+import { LOG_LEVEL } from "../constants.js";
+
+const logger = winston.createLogger({
+  level: LOG_LEVEL,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
