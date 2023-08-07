cb9d4e89cf38a1d22d4a7ce3d8f7158f9a0ad917
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-08-07 17:50:02 -0300

    chore: remove logoff end-point from userRouter

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { registerUserController, userLoginController, userLogoffController } from "../controllers/userController.js";
+import {
+  registerUserController,
+  userLoginController,
+} from "../controllers/userController.js";
 
 const userRouter = new Router();
 
 userRouter.post("/register", registerUserController);
 
 userRouter.post("/login", userLoginController);
-
-userRouter.post("/logoff", userLogoffController);
 
 export default userRouter;
