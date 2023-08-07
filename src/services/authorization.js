6540bf90e7b1712da9f1487d8e52367c5bc88786
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2023-08-07 17:42:04 -0300

    chore: delete uthorization.js file from services folder

--- src/services/authorization.js
+++
@@ -1,9 +0,0 @@
-import { getValidToken } from "../models/token.js";
-import { getUserById } from "../models/users.js";
-
-const getUserByTokenId = async (tokenId) => {
-  const token = await getValidToken(tokenId);
-  return await getUserById(token.userId);
-};
-
-export default getUserByTokenId;
